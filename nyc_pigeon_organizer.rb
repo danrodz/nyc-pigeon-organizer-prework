05a4079efacf2f5adddc18bd815161560a8920bc
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-28 01:44:50 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,8 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
   data.each do |attributes, values|
-    values.each do |array_key, names|
+    values.each do |array_key, array|
+      array.
       if !pigeon_list.has_key?(name)
         pigeon_list[name] = {
           :color => [],
