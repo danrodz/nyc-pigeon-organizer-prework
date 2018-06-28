6f8dfaa12bd7caea86a4139e450e3ccfc014eaa8
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-28 01:09:55 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |keys, values|
+    keys.each do |key, value|
+      value.each do |name|
+        if !pigeon_list.has_key?(name)
+          pigeon_list[name] = {color: [key.to_s], gender: [], lives: []}
+        end
+      end
+    end
+  end
+  pigeon_list
 end
