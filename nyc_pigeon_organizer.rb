893679120cf1f7f471e2a28efbcb0008af09ca22
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-28 01:23:27 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,9 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
-  data.each do |keys, values|
-    keys.each do |key, value|
-      key.each do |k,v|
-        puts v
-      end
+  data.each do |attributes, values|
+    values.each do |array, name|
+      
     end
   end
   pigeon_list
